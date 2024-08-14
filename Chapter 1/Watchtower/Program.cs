8f5c7c68c207dcca2bda17e581062a41dfa4d875
Raxelgrande <lucasvbm2@gmail.com>
2024-08-14 15:34:01 -0500

    Up to dating

---
+++ Chapter 1/Watchtower/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Please give a value to the X coordinate");
+string? xVal = Console.ReadLine();
+int xValInt = Convert.ToInt32(xVal);
+
+Console.WriteLine("Please give a value to the Y coordinate");
+string? yVal = Console.ReadLine();
+int yValInt = Convert.ToInt32(yVal);
+
+if (xValInt < 0 && yValInt > 0) 
+{
+    Console.WriteLine("The enemy approaches form the Northwest");
+}
+else if (xValInt == 0 && yValInt > 0) 
+{
+    Console.WriteLine("The enemy approaches form the North");
+}
+else if (xValInt > 0 && yValInt > 0) 
+{
+    Console.WriteLine("The enemy approaches form the Northeast");
+}
+else if (xValInt < 0 && yValInt == 0)
+{
+    Console.WriteLine("The enemy approaches form the West");
+}
+else if (xValInt == 0 && yValInt == 0)
+{
+    Console.WriteLine("The enemy is here!");
+}
