dc67d43e5165e3835b21d97c42bc5506ed04f18f
Lucas V. <93939943+Raxelgrande@users.noreply.github.com>
2024-09-16 13:22:23 -0500

    Delete Chapter 1 directory FUCK GIT

--- Chapter 1/Countdown/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-Recursical(10);
-
-void Recursical(int number)
-{
-    if (number == 0) return;
-    Console.WriteLine(number);
-    Recursical(number - 1);
-}
--- Chapter 1/The Triangle Farmer/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-decimal baze = 20;
-decimal height = 50;
-
-var areaCalc = baze * height / 2;
-Console.WriteLine("The area of the triangle is: " + areaCalc);
